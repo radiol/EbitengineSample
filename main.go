4d85ef6642140fb93dda002bcbbab0633f857d6f
radiol <radiol@github.com>
2025-01-12 15:11:54 +0900

    Add dependencies and implement basic game structure

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	for i := 0; i < 240; i += 16 {
+		m := fmt.Sprintf("Hello world!: %d", i)
+		ebitenutil.DebugPrintAt(screen, m, 0, i)
+	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
 func main() {
-  fmt.Println("Hello, World!")
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
