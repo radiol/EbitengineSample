22c633c03f64fb8a951baf0b8c28fdb2a47e85db
radiol <radiol@github.com>
2025-01-13 17:42:32 +0900

    Add image scaling and translation in Draw method

--- main.go
+++ main.go
@@ -29,13 +29,16 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(50, 50)
+	op.GeoM.Scale(1.5, 1.0)
 	screen.Fill(color.RGBA{0x80, 0xa0, 0xc0, 0xff})
-	screen.DrawImage(img, nil)
+	screen.DrawImage(img, op)
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("Hello, World! %d", g.count))
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return 640, 480
 }
 
 func main() {
